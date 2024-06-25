5098f65e717635f993a9b3c0f09ef2844e82a7cf
alizademhdi <alizademhdi@gmail.com>
2024-06-25 21:41:12 +0330

    Fix MSRP algorithm and add avg scheduleables

--- main.py
+++ main.py
@@ -10,30 +10,35 @@ import time
 
 def evaluate(resources, num_processors, protocol, end_time, ceilings):
     if protocol == 'MSRP':
-        simulate_msrp(resources, num_processors, end_time, ceilings)
+        scheduleable, non_scheduleable = simulate_msrp(resources, num_processors, end_time, ceilings)
     elif protocol == 'MRSP':
-        simulate_mrsp(resources, num_processors, end_time)
+        scheduleable, non_scheduleable = simulate_mrsp(resources, num_processors, end_time)
+    return scheduleable, non_scheduleable
 
 def calculate_huperperiod(tasks):
     return reduce(lcm, [t['period'] for t in tasks])
 
 if __name__ == '__main__':
-    num_tasks = 10
+    num_tasks = 40
     num_resources = 2
     processors_utilization = [0.25, 0.25]
     U_total = sum(processors_utilization)
-    min_period = 10
-    max_period = 20
+    min_period = 5
+    max_period = 15
     num_processors = 2
     max_premption_level = 3
-    max_critical_sections = 2
-    tasks = generate_tasks(num_tasks, U_total, min_period, max_period, num_resources, max_critical_sections, max_premption_level)
-    end_time = calculate_huperperiod(tasks)
-    ceilings = calculate_ceilings(tasks, num_resources)
-    processors = wfd_mapping(tasks, num_processors)
-    print_tasks(tasks)
-    print(100 * '-')
-    print(end_time)
-    time.sleep(5)
+    max_critical_sections = 1
+    num_run = 2
+    number_of_schedle_able = 0
+    avg = []
+    for _ in range(num_run):
+        tasks = generate_tasks(num_tasks, U_total, min_period, max_period, num_resources, max_critical_sections, max_premption_level)
+        end_time = calculate_huperperiod(tasks)
+        ceilings = calculate_ceilings(tasks, num_resources)
+        processors = wfd_mapping(tasks, num_processors)
+        print_tasks(tasks)
+        print(100 * '-')
 
-    evaluate(processors, num_processors, 'MSRP', end_time, ceilings)
+        scheduleable, non_scheduleable = evaluate(processors, num_processors, 'MSRP', end_time, ceilings)
+        avg.append(scheduleable/(scheduleable+non_scheduleable))
+    print(avg)
--- schedulers/MSRP.py
+++ schedulers/MSRP.py
@@ -1,15 +1,16 @@
 from tools.scheduling import*
-
 def check_deadlines(processors, time):
     for p in processors:
         for t in p:
             if t['arrival'] <= time and t['remaining_time'] > 0:
                 if t['deadline'] < time:
                     print('Task {} missed deadline'.format(t['id']))
-                    return True
-    return False
+                    return True, t
+    return False, None
 
 def simulate_msrp(processors, num_resources, end_time, ceilings):
+    num_schedulable = 0
+    num_non_scheduleable = 0
     time = 0
     resources_status = num_resources * [None]
     pi = 0
@@ -45,16 +46,16 @@ def simulate_msrp(processors, num_resources, end_time, ceilings):
                     if resources_status[c['resource']] is not None and  resources_status[c['resource']] != current_task[i]['id']:
                         current_task[i]['blocking'] = True
                         blocked = True
-                        print('Task {} is blocking for resourse {}'.format(current_task[i]['id'], c['resource']))
+                        print('Task {} is blocking for resourse {} on proceccor {}'.format(current_task[i]['id'], c['resource'], current_task[i]['processor']))
             if not blocked:
                 current_task[i]['blocking'] = False
             if current_task[i]['blocking'] is False:
                 for c in current_task[i]['criticality']:
                     if c['start'] <= current_task[i]['wcet'] - current_task[i]['remaining_time'] < c['end']:
                         resources_status[c['resource']] = current_task[i]['id']
-                        print('Task {} is using resource {}'.format(current_task[i]['id'], c['resource']))
+                        print('Task {} is using resource {} on proceccor {}'.format(current_task[i]['id'], c['resource'], current_task[i]['processor']))
                 current_task[i]['remaining_time'] -= 1
-                print('Task {} is running'.format(current_task[i]['id']))
+                print('Task {} is running on proceccor {}'.format(current_task[i]['id'], current_task[i]['processor']))
                 if current_task[i]['remaining_time'] == 0:
                     for c in current_task[i]['criticality']:
                         if resources_status[c['resource']] == current_task[i]['id']:
@@ -64,11 +65,21 @@ def simulate_msrp(processors, num_resources, end_time, ceilings):
                     current_task[i]['remaining_time'] = current_task[i]['wcet']
                     current_task[i]['blocking'] = False
                     current_task[i] = None
+                    num_schedulable += 1
             pi = max([ceilings[r] for r in range(num_resources) if resources_status[r] is not None], default=0)
-        if check_deadlines(processors, time):
-            return False
+        status, t = check_deadlines(processors, time)
+        if status:
+            num_non_scheduleable += 1
+            for c in t['criticality']:
+                if resources_status[c['resource']] == t['id']:
+                    resources_status[c['resource']] = None
+            t['arrival'] += t['period']
+            t['deadline'] += t['period']
+            t['remaining_time'] = t['wcet']
+            t['blocking'] = False
         
         time += 1
+    return num_schedulable, num_non_scheduleable
 
 
         
